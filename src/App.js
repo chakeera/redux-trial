2cb702fcdfa2b93b63e95e7b4355fa9544aced26
Chakeera <chakeera1999@gmail.com>
2021-04-14 11:33:50 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@material-ui/styles";
+import Router from "./router"
 
 
 function App() {
   return (
     <ThemeProvider>
-    
+    <Router/>
     </ThemeProvider>
   );
 }
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from '../component/PrivateRoute';
+import Landing from '../pages/Landing'
+import Home from '../pages/Home'
+import Signin from '../pages/Signin'
+import Signup from '../pages/Signup'
+
+
+const router = () => (
+    <Router>
+      <Switch>
+        <PrivateRoute path="/home" component={Home} />
+        <Route path="/" component={Landing} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </Router>
+  );
+  export default router;
